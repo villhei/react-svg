82ebdc5bd9897a7b86700c6841e06dd6351f95a8
Ville Heikkinen <ville.heikkinen@wunderdog.fi>
2018-12-13 12:02:23 +0200

    Simplify default pattern

--- src/Texts/common.tsx
+++ src/Texts/common.tsx
@@ -76,10 +76,8 @@ export type Pattern = Array<number[]>
 
 export const DEFAULT_PATTERN: Pattern = [
   [10, 20, 20],
-  [7, 50, 0],
   [11, 70, 30],
   [13, 50, 50],
-  [7, 10, 50],
   [9, 30, 70],
   [11, 70, 70]
 ]
